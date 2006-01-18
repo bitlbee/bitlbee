ac9f0e965b5002c9eb022f77eb7c6f16cba662b1
Wilmer van der Gaast <wilmer@gaast.net>
2006-01-18 08:53:50 +0100

    Formatting changes.

--- log.c
+++ log.c
@@ -37,11 +37,11 @@ static void log_console(int level, char *logmessage);
 void log_init(void) {
 	openlog("bitlbee", LOG_PID, LOG_DAEMON);	
 
-	logoutput.informational=&log_null;
-	logoutput.warning=&log_null;
-	logoutput.error=&log_null;
+	logoutput.informational = &log_null;
+	logoutput.warning = &log_null;
+	logoutput.error = &log_null;
 #ifdef DEBUG
-	logoutput.debug=&log_null;
+	logoutput.debug = &log_null;
 #endif
 
 	return;
@@ -50,46 +50,46 @@ void log_init(void) {
 void log_link(int level, int output) {
 	/* I know it's ugly, but it works and I didn't feel like messing with pointer to function pointers */
 
-	if(level==LOGLVL_INFO) {
-		if(output==LOGOUTPUT_NULL)
-			logoutput.informational=&log_null;	
-		else if(output==LOGOUTPUT_IRC)
-			logoutput.informational=&log_irc;	
-		else if(output==LOGOUTPUT_SYSLOG)
-			logoutput.informational=&log_syslog;	
-		else if(output==LOGOUTPUT_CONSOLE) 
-			logoutput.informational=&log_console;	
+	if(level == LOGLVL_INFO) {
+		if(output == LOGOUTPUT_NULL)
+			logoutput.informational = &log_null;	
+		else if(output == LOGOUTPUT_IRC)
+			logoutput.informational = &log_irc;	
+		else if(output == LOGOUTPUT_SYSLOG)
+			logoutput.informational = &log_syslog;	
+		else if(output == LOGOUTPUT_CONSOLE) 
+			logoutput.informational = &log_console;	
 	}
-	else if(level==LOGLVL_WARNING) {
-		if(output==LOGOUTPUT_NULL)
-			logoutput.warning=&log_null;
-		else if(output==LOGOUTPUT_IRC)
-			logoutput.warning=&log_irc;
-		else if(output==LOGOUTPUT_SYSLOG)
-			logoutput.warning=&log_syslog;
-		else if(output==LOGOUTPUT_CONSOLE)
-			logoutput.warning=&log_console;
+	else if(level == LOGLVL_WARNING) {
+		if(output == LOGOUTPUT_NULL)
+			logoutput.warning = &log_null;
+		else if(output == LOGOUTPUT_IRC)
+			logoutput.warning = &log_irc;
+		else if(output == LOGOUTPUT_SYSLOG)
+			logoutput.warning = &log_syslog;
+		else if(output == LOGOUTPUT_CONSOLE)
+			logoutput.warning = &log_console;
 	}
-	else if(level==LOGLVL_ERROR) {
-		if(output==LOGOUTPUT_NULL)
-			logoutput.error=&log_null;
-		else if(output==LOGOUTPUT_IRC)
-			logoutput.error=&log_irc;
-		else if(output==LOGOUTPUT_SYSLOG)
-			logoutput.error=&log_syslog;
-		else if(output==LOGOUTPUT_CONSOLE)
-			logoutput.error=&log_console;
+	else if(level == LOGLVL_ERROR) {
+		if(output == LOGOUTPUT_NULL)
+			logoutput.error = &log_null;
+		else if(output == LOGOUTPUT_IRC)
+			logoutput.error = &log_irc;
+		else if(output == LOGOUTPUT_SYSLOG)
+			logoutput.error = &log_syslog;
+		else if(output == LOGOUTPUT_CONSOLE)
+			logoutput.error = &log_console;
 	}
 #ifdef DEBUG
-	else if(level==LOGLVL_DEBUG) {
-		if(output==LOGOUTPUT_NULL)
-			logoutput.debug=&log_null;
-		else if(output==LOGOUTPUT_IRC)
-			logoutput.debug=&log_irc;
-		else if(output==LOGOUTPUT_SYSLOG)
-			logoutput.debug=&log_syslog;
-		else if(output==LOGOUTPUT_CONSOLE)
-			logoutput.debug=&log_console;
+	else if(level == LOGLVL_DEBUG) {
+		if(output == LOGOUTPUT_NULL)
+			logoutput.debug = &log_null;
+		else if(output == LOGOUTPUT_IRC)
+			logoutput.debug = &log_irc;
+		else if(output == LOGOUTPUT_SYSLOG)
+			logoutput.debug = &log_syslog;
+		else if(output == LOGOUTPUT_CONSOLE)
+			logoutput.debug = &log_console;
 	}
 #endif
 	return;	
@@ -105,14 +105,14 @@ void log_message(int level, char *message, ... ) {
 	msgstring = g_strdup_vprintf(message, ap);
 	va_end(ap);
 
-	if(level==LOGLVL_INFO)
+	if(level == LOGLVL_INFO)
 		(*(logoutput.informational))(level, msgstring);
-	if(level==LOGLVL_WARNING) 
+	if(level == LOGLVL_WARNING) 
 		(*(logoutput.warning))(level, msgstring);
-	if(level==LOGLVL_ERROR)
+	if(level == LOGLVL_ERROR)
 		(*(logoutput.error))(level, msgstring);
 #ifdef DEBUG
-	if(level==LOGLVL_DEBUG)
+	if(level == LOGLVL_DEBUG)
 		(*(logoutput.debug))(level, msgstring);
 #endif
 
@@ -132,14 +132,14 @@ static void log_null(int level, char *message) {
 }
 
 static void log_irc(int level, char *message) {
-	if(level==LOGLVL_ERROR)
+	if(level == LOGLVL_ERROR)
 		irc_write_all(1, "ERROR :Error: %s", message);
-	if(level==LOGLVL_WARNING)
+	if(level == LOGLVL_WARNING)
 		irc_write_all(0, "ERROR :Warning: %s", message);
-	if(level==LOGLVL_INFO)
+	if(level == LOGLVL_INFO)
 		irc_write_all(0, "ERROR :Informational: %s", message);	
 #ifdef DEBUG
-	if(level==LOGLVL_DEBUG)
+	if(level == LOGLVL_DEBUG)
 		irc_write_all(0, "ERROR :Debug: %s", message);	
 #endif	
 
@@ -147,28 +147,28 @@ static void log_irc(int level, char *message) {
 }
 
 static void log_syslog(int level, char *message) {
-	if(level==LOGLVL_ERROR)
+	if(level == LOGLVL_ERROR)
 		syslog(LOG_ERR, "%s", message);
-	if(level==LOGLVL_WARNING)
+	if(level == LOGLVL_WARNING)
 		syslog(LOG_WARNING, "%s", message);
-	if(level==LOGLVL_INFO)
+	if(level == LOGLVL_INFO)
 		syslog(LOG_INFO, "%s", message);
 #ifdef DEBUG
-	if(level==LOGLVL_DEBUG)
+	if(level == LOGLVL_DEBUG)
 		syslog(LOG_DEBUG, "%s", message);
 #endif
 	return;
 }
 
 static void log_console(int level, char *message) {
-	if(level==LOGLVL_ERROR)
+	if(level == LOGLVL_ERROR)
 		fprintf(stderr, "Error: %s\n", message);
-	if(level==LOGLVL_WARNING)
+	if(level == LOGLVL_WARNING)
 		fprintf(stderr, "Warning: %s\n", message);
-	if(level==LOGLVL_INFO)
+	if(level == LOGLVL_INFO)
 		fprintf(stdout, "Informational: %s\n", message);
 #ifdef DEBUG
-	if(level==LOGLVL_DEBUG)
+	if(level == LOGLVL_DEBUG)
 		fprintf(stdout, "Debug: %s\n", message);
 #endif
 	return;
