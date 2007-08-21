cd3022cce128e4ff995bd7d76b3013d1909b3332
VMiklos <vmiklos@frugalware.org>
2007-08-21 20:18:08 +0200

    skyped: added copyright header

--- skype/skyped.py
+++ skype/skyped.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python
+# 
+#   skyped.py
+#  
+#   Copyright (c) 2007 by Miklos Vajna <vmiklos@frugalware.org>
+#
+#   It uses several code from a very basic python CLI interface, available at:
+#
+#   http://forum.skype.com/index.php?showtopic=42640
+#  
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+# 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#  
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
+#   USA.
+#
+
+# makepkg configuration
 """ GPL """
 import sys
 import signal
