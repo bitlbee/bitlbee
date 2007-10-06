3ef191040a3d88b21e287cf3c64bd9fc7e8c0026
VMiklos <vmiklos@frugalware.org>
2007-10-06 22:06:14 +0200

    don't mark messages as seen - they still spams the skype client - unnecessary transfer then

--- skype/skype.c
+++ skype/skype.c
@@ -270,13 +270,10 @@ static gboolean skype_read_callback( gpointer data, gint fd, b_input_condition c
 						/* New message ID:
 						 * (1) Request its from field
 						 * (2) Request its body
-						 * (3) Mark it as seen
 						 */
 						g_snprintf(buf, 1024, "GET CHATMESSAGE %s FROM_HANDLE\n", id);
 						skype_write( ic, buf, strlen( buf ) );
 						g_snprintf(buf, 1024, "GET CHATMESSAGE %s BODY\n", id);
-						skype_write( ic, buf, strlen( buf ) );
-						g_snprintf(buf, 1024, "SET CHATMESSAGE %s SEEN\n", id);
 						skype_write( ic, buf, strlen( buf ) );
 					}
 					else if(!strncmp(info, "FROM_HANDLE ", 12))
