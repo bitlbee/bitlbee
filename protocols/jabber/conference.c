3038e476a93b2be057581b831749eac931a06559
Wilmer van der Gaast <wilmer@gaast.net>
2008-02-10 17:11:06 +0000

    Added support for password-protected Jabber chatrooms.

--- protocols/jabber/conference.c
+++ protocols/jabber/conference.c
@@ -36,6 +36,8 @@ struct groupchat *jabber_chat_join( struct im_connection *ic, char *room, char *
 	node = xt_new_node( "x", NULL, NULL );
 	xt_add_attr( node, "xmlns", XMLNS_MUC );
 	node = jabber_make_packet( "presence", NULL, roomjid, node );
+	if( password )
+		xt_add_child( node, xt_new_node( "password", password, NULL ) );
 	jabber_cache_add( ic, node, jabber_chat_join_failed );
 	
 	if( !jabber_write_packet( ic, node ) )
