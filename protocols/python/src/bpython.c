0147ca95ff14135b7e99f2313f5c8ec03daaf595
Nick Murdoch <nick@nivan.net>
2013-03-17 15:06:34 +0000

    Scan through plugindir and find python files.

--- protocols/python/src/bpython.c
+++ protocols/python/src/bpython.c
@@ -27,15 +27,50 @@
 
 void init_plugin() {
     struct prpl *ret = g_new0(struct prpl, 1);
+    GDir *dir;
+    GError *error = NULL;
 
     printf("Initialising Python... ");
     Py_Initialize();
     PyRun_SimpleString("print 'Python initialised!'\n");
-    // Py_Finalize(); // TODO - there's no plugin_unload for us to do this in.
+    
+    /* Get a reference to the main module. */
+    PyObject* main_module = PyImport_AddModule("__main__");
+
+    /* Get the main module's dictionary */
+    PyObject* main_dict = PyModule_GetDict(main_module);
+
+    dir = g_dir_open(global.conf->plugindir, 0, &error);
+    if (dir) {
+        const gchar *entry;
+        char * path;
+        while ((entry = g_dir_read_name(dir))) {
+            path = g_build_filename(global.conf->plugindir, entry, NULL);
+            if (!path) {
+                log_message(LOGLVL_WARNING, "Can't build path for %s\n", entry);
+                continue;
+            }
+            
+            if (g_str_has_suffix(path, ".py")) {
+                printf("Loading python file %s\n", path);
+                FILE* pyfile = fopen(path, "r");
+                /* Copy main dict to make sure that separate plugins
+                   run in separate environments */
+                PyObject* main_dict_copy = PyDict_Copy(main_dict);
+                PyRun_File(pyfile, path, Py_file_input, main_dict_copy, main_dict_copy);
+                
+            }
+
+            g_free(path);
+        }
+    }
+
 
     ret->name = "bpython";
 
     register_protocol(ret);
+
+    // Py_Finalize(); // TODO - there's no plugin_unload for us to do this in.
 }
 
 int main(int argc, char ** argv) {
