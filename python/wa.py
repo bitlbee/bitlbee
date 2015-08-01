e1352e15c928490517d5c0cd6e5b8130f268e1c3
Wilmer van der Gaast <wilmer@gaast.net>
2015-08-02 00:59:15 +0100

    Err. Yes, now fix those ping timeouts calls for real.

--- python/wa.py
+++ python/wa.py
@@ -353,8 +353,8 @@ class YowsupIMPlugin(implugin.BitlBeeIMPlugin):
 
 	def keepalive(self):
 		if (time.time() - self.last_pong) > self.PING_TIMEOUT:
-			self.cb.error("Ping timeout")
-			self.cb.logout(True)
+			self.bee.error("Ping timeout")
+			self.bee.logout(True)
 			return
 		if self.next_ping and (time.time() < self.next_ping):
 			return
