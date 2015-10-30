bitlbee/bitlbee

commit 7233f68b3d99c447d4f0cd855b179b9079383bb2

Improved signal handling to avoid deadlocks
- SIGSEGV: broadcast a message manually, avoiding the usual irc_write()
  functions which are unsafe due to malloc().
- SIGTERM, SIGINT: Write to a pipe which gets handled in the main loop by
  bitlbee_shutdown(), saving configs and stuff.
- SIGCHLD: set to ignore explicitly, which handles zombies correctly.
  This also drops some log messages with 'info' level, which in practice
  means they never got logged.
- SIGPIPE: set to ignore (nobody cares)
- SIGILL, SIGBUS, SIGFPE, SIGQUIT, SIGXCPU: Not handling anymore.

unix.c
@@ -47,7 +47,9 @@
 
 global_t global;	/* Against global namespace pollution */
 
-static void sighandler( int signal );
+static int signal_shutdown_pipe[2] = { -1, -1 };
+static void sighandler_shutdown( int signal );
+static void sighandler_crash( int signal );
 
 static int crypt_main( int argc, char *argv[] );
 
@@ -155,18 +157,20 @@ int main( int argc, char *argv[] )
  	
 	/* Catch some signals to tell the user what's happening before quitting */
 	memset( &sig, 0, sizeof( sig ) );
-	sig.sa_handler = sighandler;
+	sig.sa_handler = SIG_IGN;
 	sigaction( SIGCHLD, &sig, &old );
 	sigaction( SIGPIPE, &sig, &old );
 	sig.sa_flags = SA_RESETHAND;
-	sigaction( SIGINT,  &sig, &old );
-	sigaction( SIGILL,  &sig, &old );
-	sigaction( SIGBUS,  &sig, &old );
-	sigaction( SIGFPE,  &sig, &old );
+	sig.sa_handler = sighandler_crash;
 	sigaction( SIGSEGV, &sig, &old );
-	sigaction( SIGTERM, &sig, &old );
-	sigaction( SIGQUIT, &sig, &old );
-	sigaction( SIGXCPU, &sig, &old );
+
+	/* Use a pipe for SIGTERM/SIGINT so the actual signal handler doesn't do anything unsafe */
+	if ( pipe( signal_shutdown_pipe ) == 0 ) {
+		b_input_add( signal_shutdown_pipe[0], B_EV_IO_READ, bitlbee_shutdown, NULL );
+		sig.sa_handler = sighandler_shutdown;
+		sigaction( SIGINT, &sig, &old );
+		sigaction( SIGTERM, &sig, &old );
+	}
 	
 	if( !getuid() || !geteuid() )
 		log_message( LOGLVL_WARNING, "BitlBee is running with root privileges. Why?" );
@@ -258,52 +262,29 @@ static int crypt_main( int argc, char *argv[] )
 	return 0;
 }
 
-static void sighandler( int signal )
+/* Signal handler for SIGTERM and SIGINT */
+static void sighandler_shutdown( int signal )
 {
-	/* FIXME: Calling log_message() here is not a very good idea! */
-	
-	if( signal == SIGTERM || signal == SIGQUIT || signal == SIGINT )
-	{
-		static int first = 1;
-		
-		if( first )
-		{
-			/* We don't know what we were doing when this signal came in. It's not safe to touch
-			   the user data now (not to mention writing them to disk), so add a timer. */
-			
-			log_message( LOGLVL_ERROR, "SIGTERM received, cleaning up process." );
-			b_timeout_add( 1, (b_event_handler) bitlbee_shutdown, NULL );
-			
-			first = 0;
-		}
-		else
-		{
-			/* Well, actually, for now we'll never need this part because this signal handler
-			   will never be called more than once in a session for a non-SIGPIPE signal...
-			   But just in case we decide to change that: */
-			
-			log_message( LOGLVL_ERROR, "SIGTERM received twice, so long for a clean shutdown." );
-			raise( signal );
-		}
+	/* Write a single null byte to the pipe, just to send a message to the main loop.
+	 * This gets handled by bitlbee_shutdown (the b_input_add callback for this pipe) */
+	write( signal_shutdown_pipe[1], "", 1 );
+}
+
+/* Signal handler for SIGSEGV
+ * A desperate attempt to tell the user that everything is wrong in the world.
+ * Avoids using irc_abort() because it has several unsafe calls to malloc */
+static void sighandler_crash( int signal )
+{
+	GSList *l;
+	const char *message = "ERROR :BitlBee crashed! (SIGSEGV received)\r\n";
+	int len = strlen(message);
+
+	for (l = irc_connection_list; l; l = l->next ) {
+		irc_t *irc = l->data;
+		write( irc->fd, message, len );
 	}
-	else if( signal == SIGCHLD )
-	{
-		pid_t pid;
-		int st;
-		
-		while( ( pid = waitpid( 0, &st, WNOHANG ) ) > 0 )
-		{
-			if( WIFSIGNALED( st ) )
-				log_message( LOGLVL_INFO, "Client %d terminated normally. (status = %d)", (int) pid, WEXITSTATUS( st ) );
-			else if( WIFEXITED( st ) )
-				log_message( LOGLVL_INFO, "Client %d killed by signal %d.", (int) pid, WTERMSIG( st ) );
-		}
-	}
-	else if( signal != SIGPIPE )
-	{
-		log_message( LOGLVL_ERROR, "Fatal signal received: %d. That's probably a bug.", signal );
-		raise( signal );
-	}
+
+	raise( signal );
 }
 
 double gettime()

commit 2e99039dbc86bbf04320fdb2cf652e9c71a58af2

Avoid propagating shutdown signal to all subprocesses
This was a sort-of-regression with 7233f68

While this behavior might seem desirable in some cases, multi-user
installs like public servers would rather not kill children while
upgrading.

Turns out that pipes are inherited by forks, and writing in one side
means there might be more than one listener that calls
bitlbee_shutdown(). If the parent gets it, the children will get it
too. If a child gets it, the parent and the other children get it too.

This adds a sighandler_shutdown_setup() function that closes any
previously existing pipes and disconnects the events from them, to
create a new one. This is called again after forking each child process.

While I'm sure this fixes the issue, I still don't understand why it
*didn't* kill the forked processes in some cases. Worrying.

bitlbee.c
@@ -310,6 +310,9 @@ static gboolean bitlbee_io_new_client(gpointer data, gint fd, b_input_condition 
 			close(global.listen_socket);
 			b_event_remove(global.listen_watch_source_id);
 
+			/* Make a new pipe for the shutdown signal handler */
+			sighandler_shutdown_setup();
+
 			/* Make the connection. */
 			irc = irc_new(new_socket);
 

bitlbee.h
@@ -157,6 +157,8 @@ typedef struct global {
 	int restart;
 } global_t;
 
+void sighandler_shutdown_setup(void);
+
 int bitlbee_daemon_init(void);
 int bitlbee_inetd_init(void);
 

unix.c
@@ -47,7 +47,11 @@
 
 global_t global;        /* Against global namespace pollution */
 
-static int signal_shutdown_pipe[2] = { -1, -1 };
+static struct {
+	int fd[2];
+	int tag;
+} shutdown_pipe = {{-1 , -1}, 0};
+
 static void sighandler_shutdown(int signal);
 static void sighandler_crash(int signal);
 
@@ -155,13 +159,11 @@ int main(int argc, char *argv[])
 	sig.sa_handler = sighandler_crash;
 	sigaction(SIGSEGV, &sig, &old);
 
-	/* Use a pipe for SIGTERM/SIGINT so the actual signal handler doesn't do anything unsafe */
-	if (pipe(signal_shutdown_pipe) == 0) {
-		b_input_add(signal_shutdown_pipe[0], B_EV_IO_READ, bitlbee_shutdown, NULL);
-		sig.sa_handler = sighandler_shutdown;
-		sigaction(SIGINT, &sig, &old);
-		sigaction(SIGTERM, &sig, &old);
-	}
+	sighandler_shutdown_setup();
+
+	sig.sa_handler = sighandler_shutdown;
+	sigaction(SIGINT, &sig, &old);
+	sigaction(SIGTERM, &sig, &old);
 
 	if (!getuid() || !geteuid()) {
 		log_message(LOGLVL_WARNING, "BitlBee is running with root privileges. Why?");
@@ -255,12 +257,27 @@ static int crypt_main(int argc, char *argv[])
 	return 0;
 }
 
+/* Set up a pipe for SIGTERM/SIGINT so the actual signal handler doesn't do anything unsafe */
+void sighandler_shutdown_setup()
+{
+	if (shutdown_pipe.fd[0] != -1) {
+		/* called again from a forked process, clean up to avoid propagating the signal */
+		b_event_remove(shutdown_pipe.tag);
+		close(shutdown_pipe.fd[0]);
+		close(shutdown_pipe.fd[1]);
+	}
+
+	if (pipe(shutdown_pipe.fd) == 0) {
+		shutdown_pipe.tag = b_input_add(shutdown_pipe.fd[0], B_EV_IO_READ, bitlbee_shutdown, NULL);
+	}
+}
+
 /* Signal handler for SIGTERM and SIGINT */
 static void sighandler_shutdown(int signal)
 {
 	/* Write a single null byte to the pipe, just to send a message to the main loop.
 	 * This gets handled by bitlbee_shutdown (the b_input_add callback for this pipe) */
-	write(signal_shutdown_pipe[1], "", 1);
+	write(shutdown_pipe.fd[1], "", 1);
 }
 
 /* Signal handler for SIGSEGV