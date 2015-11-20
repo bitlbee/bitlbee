365b25a2e4eca428cf053f5e7754ae990bcf053b
Wilmer van der Gaast <wilmer@gaast.net>
2015-11-20 08:18:12 -0800

    One callback ref fix, and show at least something for messages of an unknown type. (Currently there's no handling for audio msgs for example.)

--- python/wa.py
+++ python/wa.py
@@ -279,7 +279,7 @@ class BitlBeeLayer(YowInterfaceLayer):
 
 	def onStatusNotification(self, status):
 		print "New status for %s: %s" % (status.getFrom(), status.status)
-		self.bee.buddy_status_msg(status.getFrom(), status.status)
+		self.cb.buddy_status_msg(status.getFrom(), status.status)
 	
 	def onGroupSubjectNotification(self, sub):
 		print "New /topic for %s: %s" % (sub.getFrom(), sub.getSubject())
@@ -438,7 +438,7 @@ class YowsupIMPlugin(implugin.BitlBeeIMPlugin):
 		creds = (account["user"].split("@")[0], account["pass"])
 
 		stack = (YowStackBuilder()
-		         .pushDefaultLayers(False)
+		         .pushDefaultLayers(True)
 		         .push(BitlBeeLayer)
 		         .build())
 		stack.setProp(YowAuthenticationProtocolLayer.PROP_CREDENTIALS, creds)
@@ -472,6 +472,8 @@ class YowsupIMPlugin(implugin.BitlBeeIMPlugin):
 			if msg.getCaption():
 				lines.append(msg.getCaption())
 			text = "\n".join(lines)
+		else:
+			text = "Message of unknown type %r" % type(msg)
 
 		if msg.getParticipant():
 			group = self.groups[msg.getFrom()]
