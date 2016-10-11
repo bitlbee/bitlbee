57274a128cc367cac3817d53704b1fa44571a740
Stefan <stefan.helmert@t-online.de>
2016-10-11 21:10:32 +0000

    auto installer ready and working

--- install.bash
+++ install.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt update
-apt install libssl-dev
+apt install libssl-dev pkg-config libglib2.0-dev
 ./configure --ssl=openssl && make && make install && make install-etc && make install-dev
 
