bitlbee/bitlbee

commit d57484d99125eb602e71629d7cacf76fc36cdd9c

Change some asserts into g_return_if_fail()
Because crashing asserts are bad, and maybe this helps fix the
captures_build_path issue with debian's reproducible builds
(those asserts probably include __FILE__)

protocols/oscar/rxhandlers.c
@@ -234,14 +234,8 @@ int aim_conn_addhandler(aim_session_t *sess, aim_conn_t *conn, guint16 family, g
 {
 	struct aim_rxcblist_s *newcb;
 
-	if (!conn) {
-		return -1;
-	}
-
-	if (checkdisallowed(family, type)) {
-		g_assert(0);
-		return -1;
-	}
+	g_return_val_if_fail(conn, -1);
+	g_return_val_if_fail(checkdisallowed(family, type), -1);
 
 	if (!(newcb = (struct aim_rxcblist_s *) g_new0(struct aim_rxcblist_s, 1))) {
 		return -1;

otr.c
@@ -1513,7 +1513,7 @@ struct im_connection *check_imc(void *opdata, const char *accountname,
 				break;
 			}
 		}
-		assert(l != NULL);  /* a match should always be found */
+		g_return_val_if_fail(l, NULL);
 		if (!l) {
 			return NULL;
 		}

protocols/purple/purple.c
@@ -1689,8 +1689,8 @@ void purple_initmodule()
 		return;
 	}
 
-	g_assert((int) B_EV_IO_READ == (int) PURPLE_INPUT_READ);
-	g_assert((int) B_EV_IO_WRITE == (int) PURPLE_INPUT_WRITE);
+	g_return_if_fail((int) B_EV_IO_READ == (int) PURPLE_INPUT_READ);
+	g_return_if_fail((int) B_EV_IO_WRITE == (int) PURPLE_INPUT_WRITE);
 
 	dir = g_strdup_printf("%s/purple", global.conf->configdir);
 	purple_util_set_user_dir(dir);

commit 188b75e113480aa4237d77927e3f53993ac4992a

oscar: fix checkdisallowed condition, which was breaking login.
Accidentally broke it in d57484d (from last week)

protocols/oscar/rxhandlers.c
@@ -235,7 +235,7 @@ int aim_conn_addhandler(aim_session_t *sess, aim_conn_t *conn, guint16 family, g
 	struct aim_rxcblist_s *newcb;
 
 	g_return_val_if_fail(conn, -1);
-	g_return_val_if_fail(checkdisallowed(family, type), -1);
+	g_return_val_if_fail(!checkdisallowed(family, type), -1);
 
 	if (!(newcb = (struct aim_rxcblist_s *) g_new0(struct aim_rxcblist_s, 1))) {
 		return -1;