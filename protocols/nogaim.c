bitlbee/bitlbee

commit ac68733ad7a5127395d6367b655a2d9de5d911b5

If imc_away_state_find() fails, try again finding "away"
Fixes trac ticket 1175, https://bugs.bitlbee.org/bitlbee/ticket/1175
"Setting away with libpurple fails silently except for a few values"

Turns out the fallback was the first item of the list, which for
libpurple's jabber is "Feeling" (what?) and seems to be a no-op in
practice.

With this commit the fallbacks are in this order:

1. Try to find whatever the user wrote in the away setting.
2. If that fails, find "away" in the away states.
3. If that fails, use the first away state.

This might not fix it in some rare cases where the prpl doesn't have
an away state equivalent to away. But hopefully the old fallback works
for those, jabber was the weird one here.

protocols/nogaim.c
@@ -621,7 +621,8 @@ int imc_away_send_update(struct im_connection *ic)
 	if (away && *away) {
 		GList *m = ic->acc->prpl->away_states(ic);
 		msg = ic->acc->flags & ACC_FLAG_AWAY_MESSAGE ? away : NULL;
-		away = imc_away_state_find(m, away, &msg) ? : m->data;
+		away = imc_away_state_find(m, away, &msg) ? :
+		       (imc_away_state_find(m, "away", &msg) ? : m->data);
 	} else if (ic->acc->flags & ACC_FLAG_STATUS_MESSAGE) {
 		away = NULL;
 		msg = set_getstr(&ic->acc->set, "status") ?

commit eb73d05e00156afd5ea6d7819d0d7ef2ccadedde

Fix status message being set to null accidentally
In other words, "account jabber set away Message" resulted in away
status being set, but not "Message".

Fixes trac ticket 1291

This was broken by ac68733ad7a5127395d6367b655a2d9de5d911b5 (3.4.2)

That commit added another call of imc_away_state_find() with the fixed
string "away", so purple protocols can become away by setting away to
any string. But by doing so, it wiped the status message because it's
the same as the name of the away state.

protocols/nogaim.c
@@ -767,7 +767,7 @@ int imc_away_send_update(struct im_connection *ic)
 		}
 		msg = ic->acc->flags & ACC_FLAG_AWAY_MESSAGE ? away : NULL;
 		away = imc_away_state_find(m, away, &msg) ? :
-		       (imc_away_state_find(m, "away", &msg) ? : m->data);
+		       (imc_away_state_find(m, "away", NULL) ? : m->data);
 	} else if (ic->acc->flags & ACC_FLAG_STATUS_MESSAGE) {
 		away = NULL;
 		msg = set_getstr(&ic->acc->set, "status") ?
@@ -801,7 +801,7 @@ static char *imc_away_state_find(GList *gcm, char *away, char **message)
 			/* At least the Yahoo! module works better if message
 			   contains no data unless it adds something to what
 			   we have in state already. */
-			if (strlen(m->data) == strlen(away)) {
+			if (message && strlen(m->data) == strlen(away)) {
 				*message = NULL;
 			}
 
@@ -827,7 +827,7 @@ static char *imc_away_state_find(GList *gcm, char *away, char **message)
 		for (j = 0; imc_away_alias_list[i][j]; j++) {
 			for (m = gcm; m; m = m->next) {
 				if (g_strcasecmp(imc_away_alias_list[i][j], m->data) == 0) {
-					if (!keep_message) {
+					if (!keep_message && message) {
 						*message = NULL;
 					}
 