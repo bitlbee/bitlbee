ef2cbca88d6d882d9d195efc71e5a02529c37fe6
/ <>
2021-02-25 18:37:08 +0100

    more testing

--- .tests/test1.py
+++ .tests/test1.py
@@ -7,20 +7,24 @@ class ircClient:
         self.log = ''
         self.sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def sendRaw(self, msg):
+    def sendRaw(self, msg, loud = True):
         self.receive()
+        if loud:
+            print('FROM '+ self.nick + '|| ' + msg)
         self.log += msg+'\r\n'
         self.sck.send((msg+'\r\n').encode())
 
-    def sendPrivMsg(self, recip, msg):
-        self.sendRaw('PRIVMSG '+recip+' :'+msg)
+    def sendPrivMsg(self, recip, msg, loud = True):
+        self.sendRaw('PRIVMSG '+recip+' :'+msg, loud);)
 
     def connect(self):
         try:
             self.sck.connect(('127.0.0.1', 6667))
         except:
-            print("Connection failed")
+            print("IRC connection failed for " + self.nick)
             sys.exit(1)
+        
+        print("IRC connection established for " + self.nick)
 
         self.sendRaw('USER ' + (self.nick + " ")*3)
         self.sendRaw('NICK ' + self.nick)
@@ -33,8 +37,10 @@ class ircClient:
         time.sleep(1)
         self.receive()
         if self.log.find('Logged in') == -1:
-            print("Jabber Login failed")
+            print("Jabber login failed for " + self.nick)
             sys.exit(1)
+        else:
+            print("Jabber login successful for " + self.nick)
 
     def receive(self):
         text = ''
@@ -52,13 +58,26 @@ class ircClient:
 
     def addJabberBuddy(self, nick):
         self.sendPrivMsg("&bitlbee", "add 0 " + nick+"@localhost")
+    
+    def blockJabberBuddy(self, nick):
+        self.sendPrivMsg("&bitlbee", "block " + nick)
 
-    def acceptJabberRequest(self):
-        recent = self.receive()
-        if recent.find("not in your buddy list") == -1:
-            print("No request found")
-            return
-        self.sendPrivMsg("&bitlbee", "yes")
+    def unblockJabberBuddy(self, nick):
+        self.sendPrivMsg("&bitlbee", "allow " + nick)
+
+    def renameJabberBuddy(self, oldnick, newnick)
+        self.sendPrivMsg("&bitlbee", "rename " + oldnick + " " + newnick)
+        
+def testSendMessage(sender, receiver, message, shouldreceive = True):
+    sender.sendPrivMsg(b.nick, message)
+    msginhere = receiver.receive()
+    received = !(msginhere.find(message) == -1)
+    if shouldreceive ^ received:
+        print('Test failed: Message from ' + sender.nick + ' to ' + receiver.nick)
+        print('Sender Log:' + a.log)
+        print('Receiver Log:' + b.log)
+        sys.exit(1)
+    else:
 
 def runTests():
     clis = []
@@ -67,20 +86,22 @@ def runTests():
     for cli in clis:
         cli.connect()
         cli.jabberLogin()
-    a, b = clis[0], clis[1]
 
-    a.addJabberBuddy(b.nick)
-    b.acceptJabberRequest()
-    a.sendPrivMsg(b.nick, 'ohai qtie')
+    clis[0].addJabberBuddy(clis[1].nick)
 
-    a.receive()
-    b.receive()
-    if b.log.find('ohai qtie') == -1:
-        print('Message not coming through')
-        print('Sender Log:' + a.log)
-        print('Receiver Log:' + b.log)
-        sys.exit(1)
-    print("Message test successful")
+    print("Test: Send message")
+    testSendMessage(clis[0],clis[1]),'ohai <3')
+    testSendMessage(clis[1],clis[0]),'uwu *pounces*')
+    print("Test passed")
+
+    print("Test: Block/Unblock")
+    clis[0].blockJabberBuddy(clis[1].nick)
+    testSendMessage(clis[1],clis[0]),'m-meow?', shouldreceive = False)
+    clis[0].unblockJabberBuddy(clis[1].nick)
+    testSendMessage(clis[1],clis[0]),'*purrs*')
+    print("Test passed")
+    
+    
 
 if __name__ == "__main__":
     runTests()
