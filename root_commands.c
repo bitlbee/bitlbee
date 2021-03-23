bitlbee/bitlbee

commit 6908ab747d1eb461c8b8e666113aed47924f5b58

Add 'plugins info' subcommand, only show plugin details there

root_commands.c
@@ -1163,35 +1163,70 @@ static void prplstr(GList *prpls, GString *gstr)
 	g_list_free(prpls);
 }
 
+static void cmd_plugins_info(irc_t *irc, char **cmd)
+{
+	GList *l;
+	struct plugin_info *info;
+
+	MIN_ARGS(2);
+
+	for (l = get_plugins(); l; l = l->next) {
+		info = l->data;
+		if (g_strcasecmp(cmd[2], info->name) == 0) {
+			break;
+		}
+	}
+
+	if (!l) {
+		return;
+	}
+
+	irc_rootmsg(irc, "%s:", info->name);
+	irc_rootmsg(irc, "  Version: %s", info->version);
+
+	if (info->description) {
+		irc_rootmsg(irc, "  Description: %s", info->description);
+	}
+
+	if (info->author) {
+		irc_rootmsg(irc, "  Author: %s", info->author);
+	}
+
+	if (info->url) {
+		irc_rootmsg(irc, "  URL: %s", info->url);
+	}
+}
+
 static void cmd_plugins(irc_t *irc, char **cmd)
 {
 	GList *prpls;
 	GString *gstr;
 
+	if (cmd[1] && g_strcasecmp(cmd[1], "info") == 0) {
+		cmd_plugins_info(irc, cmd);
+		return;
+	}
+
 #ifdef WITH_PLUGINS
 	GList *l;
 	struct plugin_info *info;
+	char *format;
+
+	if (strchr(irc->umode, 'b') != NULL) {
+		format = "%s\t%s";
+	} else {
+		format = "%-30s  %s";
+	}
+
+	irc_rootmsg(irc, format, "Plugin", "Version");
 
 	for (l = get_plugins(); l; l = l->next) {
 		info = l->data;
-		irc_rootmsg(irc, "%s:", info->name);
-		irc_rootmsg(irc, "  Version: %s", info->version);
-
-		if (info->description) {
-			irc_rootmsg(irc, "  Description: %s", info->description);
-		}
-
-		if (info->author) {
-			irc_rootmsg(irc, "  Author: %s", info->author);
-		}
-
-		if (info->url) {
-			irc_rootmsg(irc, "  URL: %s", info->url);
-		}
-
-		irc_rootmsg(irc, "");
+		irc_rootmsg(irc, format, info->name, info->version);
 	}
 #endif
+
+	irc_rootmsg(irc, "");
 
 	gstr = g_string_new(NULL);
 	prpls = get_protocols();

doc/user-guide/commands.xml
@@ -1868,11 +1868,15 @@
 
 	<bitlbee-command name="plugins">
 		<short-description>List all the external plugins and protocols</short-description>
-		<syntax>plugins</syntax>
+		<syntax>plugins [info &lt;name&gt;]</syntax>
 
 		<description>
 			<para>
 				This gives you a list of all the external plugins and protocols.
+			</para>
+
+			<para>
+				Use the <emphasis>info</emphasis> subcommand to get more details about a plugin.
 			</para>
 		</description>
 

commit a8196d6f72b44be9c315b9ad7dc45276d2aaca0c

root_commands: prevent linker error when plugins are disabled
This commit fixes compilation issue with disabled plugin support
(./configure --plugins=0), where get_plugins function is unavailable.
The problem has been introduced with addition of new 'plugins info'
subcommand, where get_plugins is used in cmd_plugins_info function,
which should be conditionally available only if WITH_PLUGINS is defined.

Bug: https://bugs.gentoo.org/739510
Bug: https://bugs.gentoo.org/617604
Fixes: 6908ab747d1e ("Add 'plugins info' subcommand, only show plugin details there")
Signed-off-by: Petr Vaněk <arkamar@atlas.cz>

root_commands.c
@@ -1168,6 +1168,7 @@ static void prplstr(GList *prpls, GString *gstr)
 	g_list_free(prpls);
 }
 
+#ifdef WITH_PLUGINS
 static void cmd_plugins_info(irc_t *irc, char **cmd)
 {
 	GList *l;
@@ -1201,6 +1202,7 @@ static void cmd_plugins_info(irc_t *irc, char **cmd)
 		irc_rootmsg(irc, "  URL: %s", info->url);
 	}
 }
+#endif
 
 static void cmd_plugins(irc_t *irc, char **cmd)
 {
@@ -1208,7 +1210,9 @@ static void cmd_plugins(irc_t *irc, char **cmd)
 	GString *gstr;
 
 	if (cmd[1] && g_strcasecmp(cmd[1], "info") == 0) {
+#ifdef WITH_PLUGINS
 		cmd_plugins_info(irc, cmd);
+#endif
 		return;
 	}
 