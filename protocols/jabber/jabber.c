4c075d13d21842bd23e46a03e25234401a60658f
/ <>
2021-03-26 14:26:30 +0100

    less more violence

--- protocols/jabber/jabber.c
+++ protocols/jabber/jabber.c
@@ -329,8 +329,8 @@ static void jabber_logout(struct im_connection *ic)
 	while (jd->streamhosts) {
 		jabber_streamhost_t *sh = jd->streamhosts->data;
 		jd->streamhosts = g_slist_remove(jd->streamhosts, sh);
-		g_free(sh->jid);
-		g_free(sh->host);
+//		g_free(sh->jid);
+//		g_free(sh->host);
 		g_free(sh);
 	}
 
@@ -379,7 +379,7 @@ static void jabber_logout(struct im_connection *ic)
 //	g_free(jd->muc_host);
 //	g_free(jd->username);
 //	g_free(jd->me);
-//	g_free(jd);
+	g_free(jd);
 
 	jabber_connections = g_slist_remove(jabber_connections, ic);
 }
