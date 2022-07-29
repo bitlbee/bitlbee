b86f37d3d5d043ecca673eeee50632d736aaf83a
Petr Vaněk <arkamar@atlas.cz>
2022-07-29 14:32:01 +0200

    Fix error type-punned pointer warning
    Dereferencing type-punned pointer will break strict-aliasing rules
    
    Based on upstream[1] commits.
    
    [1] https://github.com/json-parser/json-parser
    
    Based-on: 161dff8321df ("Fix error type-punned pointer warning")
    Based-on: d7b67db02aaa ("Fix error type-punned pointer warning")
    Bug: https://bugs.gentoo.org/861371
    Signed-off-by: Petr Vaněk <arkamar@atlas.cz>

--- lib/json.c
+++ lib/json.c
@@ -139,7 +139,7 @@ static int new_value
 				return 0;
 			}
 
-			value->_reserved.object_mem = (*(char **) &value->u.object.values) + values_size;
+			value->_reserved.object_mem = (void *) (((char *) value->u.object.values) + values_size);
 
 			value->u.object.length = 0;
 			break;
@@ -406,7 +406,8 @@ json_value * json_parse_ex(json_settings * settings,
 					case json_object:
 
 						if (state.first_pass) {
-							(*(json_char **) &top->u.object.values) += string_length + 1;
+							json_char **chars = (json_char **) &top->u.object.values;
+							chars[0] += string_length + 1;
 						} else {
 							top->u.object.values [top->u.object.length].name
 							        = (json_char *) top->_reserved.object_mem;
