1bdc6696aefb617873b56001f81d649d6ca3aa8e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-02-23 23:48:10 +0000

    Migrate internal users of md5.h to using GChecksum directly (#169)
    * Use GChecksum directly rather than md5 wrapper
    
    * Mark md5 functions as deprecated.
    
    * Migrate more users of md5.h to GChecksum

--- lib/md5.h
+++ lib/md5.h
@@ -7,13 +7,18 @@
 typedef guint8 md5_byte_t;
 typedef GChecksum *md5_state_t;
 
-
 #define MD5_HASH_SIZE 16
 
-void md5_init(md5_state_t *);
-void md5_append(md5_state_t *, const guint8 *, unsigned int);
-void md5_finish(md5_state_t *, guint8 digest[MD5_HASH_SIZE]);
-void md5_digest_keep(md5_state_t *, guint8 digest[MD5_HASH_SIZE]);
-void md5_free(md5_state_t *);
+#ifdef __GNUC__
+#define __MD5_NON_PUBLIC_DEPRECATION__ __attribute__((deprecated("md5.h will be removed from Bitlbee's public API. Please use another library (such as GLib's gchecksum) instead")))
+#else
+#define __MD5_NON_PUBLIC_DEPRECATION__
+#endif
+
+void md5_init(md5_state_t *) __MD5_NON_PUBLIC_DEPRECATION__;
+void md5_append(md5_state_t *, const guint8 *, unsigned int) __MD5_NON_PUBLIC_DEPRECATION__;
+void md5_finish(md5_state_t *, guint8 digest[MD5_HASH_SIZE]) __MD5_NON_PUBLIC_DEPRECATION__;
+void md5_digest_keep(md5_state_t *, guint8 digest[MD5_HASH_SIZE]) __MD5_NON_PUBLIC_DEPRECATION__;
+void md5_free(md5_state_t *) __MD5_NON_PUBLIC_DEPRECATION__;
 
 #endif
