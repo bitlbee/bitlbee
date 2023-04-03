fff28da29ea58fc41c7cd2c55fb7bb93536568da
Robert Scheck <robert@fedoraproject.org>
2023-04-03 20:01:08 +0200

    HTTPSify BitlBee URLs
    ...and refer to GitHub instead of Bazaar.

--- bitlbee.h
+++ bitlbee.h
@@ -124,7 +124,7 @@ extern "C" {
 
 #define DEFAULT_AWAY "Away from computer"
 #define CONTROL_TOPIC "Welcome to the control channel. Type \2help\2 for help information."
-#define IRCD_INFO PACKAGE " <http://www.bitlbee.org/>"
+#define IRCD_INFO PACKAGE " <https://www.bitlbee.org/>"
 
 #define MAX_NICK_LENGTH 24
 
