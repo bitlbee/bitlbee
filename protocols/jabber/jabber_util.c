bitlbee/bitlbee

commit 1bdc6696aefb617873b56001f81d649d6ca3aa8e

Migrate internal users of md5.h to using GChecksum directly (#169)
* Use GChecksum directly rather than md5 wrapper

* Mark md5 functions as deprecated.

* Migrate more users of md5.h to GChecksum

protocols/jabber/jabber_util.c
@@ -22,7 +22,6 @@
 \***************************************************************************/
 
 #include "jabber.h"
-#include "md5.h"
 #include "base64.h"
 
 static unsigned int next_id = 1;
@@ -140,15 +139,18 @@ void jabber_cache_add(struct im_connection *ic, struct xt_node *node, jabber_cac
 {
 	struct jabber_data *jd = ic->proto_data;
 	struct jabber_cache_entry *entry = g_new0(struct jabber_cache_entry, 1);
-	md5_state_t id_hash;
-	md5_byte_t id_sum[16];
+	GChecksum *id_hash;
+	gsize digest_len = MD5_HASH_SIZE;
+	guint8 id_sum[MD5_HASH_SIZE];
 	char *id, *asc_hash;
 
 	next_id++;
 
 	id_hash = jd->cached_id_prefix;
-	md5_append(&id_hash, (md5_byte_t *) &next_id, sizeof(next_id));
-	md5_digest_keep(&id_hash, id_sum);
+	g_checksum_update(id_hash, (guint8 *) &next_id, sizeof(next_id));
+	g_checksum_get_digest(id_hash, id_sum, &digest_len);
+	g_checksum_free(id_hash);
+
 	asc_hash = base64_encode(id_sum, 12);
 
 	id = g_strdup_printf("%s%s", JABBER_CACHED_ID, asc_hash);

lib/md5.c
@@ -27,9 +27,11 @@ void md5_finish(md5_state_t *ctx, guint8 digest[MD5_HASH_SIZE])
  * and finishes that one instead of the original */
 void md5_digest_keep(md5_state_t *ctx, guint8 digest[MD5_HASH_SIZE])
 {
-	md5_state_t copy = g_checksum_copy(*ctx);
+	gsize digest_len = MD5_HASH_SIZE;
+	GChecksum *copy = g_checksum_copy(*ctx);
 
-	md5_finish(&copy, digest);
+	g_checksum_get_digest(copy, digest, &digest_len);
+	g_checksum_free(copy);
 }
 
 void md5_free(md5_state_t *ctx)

lib/md5.h
@@ -7,13 +7,18 @@
 typedef guint8 md5_byte_t;
 typedef GChecksum *md5_state_t;
 
-
 #define MD5_HASH_SIZE 16
 
-void md5_init(md5_state_t *);
-void md5_append(md5_state_t *, const guint8 *, unsigned int);
-void md5_finish(md5_state_t *, guint8 digest[MD5_HASH_SIZE]);
-void md5_digest_keep(md5_state_t *, guint8 digest[MD5_HASH_SIZE]);
-void md5_free(md5_state_t *);
+#ifdef __GNUC__
+#define __MD5_NON_PUBLIC_DEPRECATION__ __attribute__((deprecated("md5.h will be removed from Bitlbee's public API. Please use another library (such as GLib's gchecksum) instead")))
+#else
+#define __MD5_NON_PUBLIC_DEPRECATION__
+#endif
+
+void md5_init(md5_state_t *) __MD5_NON_PUBLIC_DEPRECATION__;
+void md5_append(md5_state_t *, const guint8 *, unsigned int) __MD5_NON_PUBLIC_DEPRECATION__;
+void md5_finish(md5_state_t *, guint8 digest[MD5_HASH_SIZE]) __MD5_NON_PUBLIC_DEPRECATION__;
+void md5_digest_keep(md5_state_t *, guint8 digest[MD5_HASH_SIZE]) __MD5_NON_PUBLIC_DEPRECATION__;
+void md5_free(md5_state_t *) __MD5_NON_PUBLIC_DEPRECATION__;
 
 #endif

lib/misc.c
@@ -33,7 +33,6 @@
 #define BITLBEE_CORE
 #include "nogaim.h"
 #include "base64.h"
-#include "md5.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -46,7 +45,6 @@
 #include <resolv.h>
 #endif
 
-#include "md5.h"
 #include "ssl_client.h"
 
 void strip_linefeed(gchar *text)
@@ -575,16 +573,18 @@ gboolean ssl_sockerr_again(void *ssl)
                     1 == Password doesn't match the hash. */
 int md5_verify_password(char *password, char *hash)
 {
-	md5_byte_t *pass_dec = NULL;
-	md5_byte_t pass_md5[16];
-	md5_state_t md5_state;
+	guint8 *pass_dec = NULL;
+	guint8 pass_md5[16];
+	GChecksum *md5_state;
+	gsize digest_len = MD5_HASH_SIZE;
 	int ret = -1, i;
 
 	if (base64_decode(hash, &pass_dec) == 21) {
-		md5_init(&md5_state);
-		md5_append(&md5_state, (md5_byte_t *) password, strlen(password));
-		md5_append(&md5_state, (md5_byte_t *) pass_dec + 16, 5);  /* Hmmm, salt! */
-		md5_finish(&md5_state, pass_md5);
+		md5_state = g_checksum_new(G_CHECKSUM_MD5);
+		g_checksum_update(md5_state, (guint8 *) password, strlen(password));
+		g_checksum_update(md5_state, (guint8 *) pass_dec + 16, 5);  /* Hmmm, salt! */
+		g_checksum_get_digest(md5_state, pass_md5, &digest_len);
+		g_checksum_free(md5_state);
 
 		for (i = 0; i < 16; i++) {
 			if (pass_dec[i] != pass_md5[i]) {

protocols/jabber/jabber.c
@@ -32,7 +32,6 @@
 #include "bitlbee.h"
 #include "jabber.h"
 #include "oauth.h"
-#include "md5.h"
 
 GSList *jabber_connections;
 
@@ -300,20 +299,20 @@ void jabber_connect(struct im_connection *ic)
 	jabber_generate_id_hash(jd);
 }
 
-/* This generates an unfinished md5_state_t variable. Every time we generate
+/* This generates an unfinished g_checksum variable. Every time we generate
    an ID, we finish the state by adding a sequence number and take the hash. */
 static void jabber_generate_id_hash(struct jabber_data *jd)
 {
-	md5_byte_t binbuf[4];
+	guint8 binbuf[4];
 	char *s;
 
-	md5_init(&jd->cached_id_prefix);
-	md5_append(&jd->cached_id_prefix, (unsigned char *) jd->username, strlen(jd->username));
-	md5_append(&jd->cached_id_prefix, (unsigned char *) jd->server, strlen(jd->server));
+	jd->cached_id_prefix = g_checksum_new(G_CHECKSUM_MD5);
+	g_checksum_update(jd->cached_id_prefix, jd->username, strlen(jd->username));
+	g_checksum_update(jd->cached_id_prefix, jd->server, strlen(jd->server));
 	s = set_getstr(&jd->ic->acc->set, "resource");
-	md5_append(&jd->cached_id_prefix, (unsigned char *) s, strlen(s));
+	g_checksum_update(jd->cached_id_prefix, s, strlen(s));
 	random_bytes(binbuf, 4);
-	md5_append(&jd->cached_id_prefix, binbuf, 4);
+	g_checksum_update(jd->cached_id_prefix, binbuf, 4);
 }
 
 static void jabber_logout(struct im_connection *ic)
@@ -370,7 +369,7 @@ static void jabber_logout(struct im_connection *ic)
 
 	xt_free(jd->xt);
 
-	md5_free(&jd->cached_id_prefix);
+	g_checksum_free(jd->cached_id_prefix);
 
 	g_free(jd->oauth2_access_token);
 	g_free(jd->away_message);

protocols/jabber/jabber.h
@@ -27,7 +27,6 @@
 #include <glib.h>
 
 #include "bitlbee.h"
-#include "md5.h"
 #include "xmltree.h"
 
 extern GSList *jabber_connections;
@@ -104,7 +103,7 @@ struct jabber_data {
 	guint64 gmail_time;
 	char *gmail_tid;
 
-	md5_state_t cached_id_prefix;
+	GChecksum *cached_id_prefix;
 	GHashTable *node_cache;
 	GHashTable *buddies;
 

protocols/jabber/sasl.c
@@ -301,8 +301,9 @@ xt_status sasl_pkt_challenge(struct xt_node *node, gpointer data)
 
 	if (!(s = sasl_get_part(dec, "rspauth"))) {
 		/* See RFC 2831 for for information. */
-		md5_state_t A1, A2, H;
-		md5_byte_t A1r[16], A2r[16], Hr[16];
+		GChecksum *A1, *A2, *H;
+		gsize digest_len = MD5_HASH_SIZE;
+		guint8 A1r[16], A2r[16], Hr[16];
 		char A1h[33], A2h[33], Hh[33];
 		int i;
 
@@ -326,39 +327,45 @@ xt_status sasl_pkt_challenge(struct xt_node *node, gpointer data)
 
 		/* Generate the MD5 hash of username:realm:password,
 		   I decided to call it H. */
-		md5_init(&H);
+		H = g_checksum_new(G_CHECKSUM_MD5);
 		s = g_strdup_printf("%s:%s:%s", jd->username, realm, ic->acc->pass);
-		md5_append(&H, (unsigned char *) s, strlen(s));
+		g_checksum_update(H, (guint8 *)s, strlen(s));
 		g_free(s);
-		md5_finish(&H, Hr);
+
+		g_checksum_get_digest(H, Hr, &digest_len);
+		g_checksum_free(H);
 
 		/* Now generate the hex. MD5 hash of H:nonce:cnonce, called A1. */
-		md5_init(&A1);
+		A1 = g_checksum_new(G_CHECKSUM_MD5);
 		s = g_strdup_printf(":%s:%s", nonce, cnonce);
-		md5_append(&A1, Hr, 16);
-		md5_append(&A1, (unsigned char *) s, strlen(s));
+		g_checksum_update(A1, Hr, 16);
+		g_checksum_update(A1, (guint8 *)s, strlen(s));
 		g_free(s);
-		md5_finish(&A1, A1r);
+		g_checksum_get_digest(A1, A1r, &digest_len);
+		g_checksum_free(A1);
 		for (i = 0; i < 16; i++) {
 			sprintf(A1h + i * 2, "%02x", A1r[i]);
 		}
 
 		/* A2... */
-		md5_init(&A2);
+		A2 = g_checksum_new(G_CHECKSUM_MD5);
 		s = g_strdup_printf("%s:%s", "AUTHENTICATE", digest_uri);
-		md5_append(&A2, (unsigned char *) s, strlen(s));
+		g_checksum_update(A2, (guint8 *)s, strlen(s));
 		g_free(s);
-		md5_finish(&A2, A2r);
+		g_checksum_get_digest(A2, A2r, &digest_len);
+		g_checksum_free(A2);
 		for (i = 0; i < 16; i++) {
 			sprintf(A2h + i * 2, "%02x", A2r[i]);
 		}
 
 		/* Final result: A1:nonce:00000001:cnonce:auth:A2. Let's reuse H for it. */
-		md5_init(&H);
+		H = g_checksum_new(G_CHECKSUM_MD5);
 		s = g_strdup_printf("%s:%s:%s:%s:%s:%s", A1h, nonce, "00000001", cnonce, "auth", A2h);
-		md5_append(&H, (unsigned char *) s, strlen(s));
+		g_checksum_update(H, (guint8 *)s, strlen(s));
 		g_free(s);
-		md5_finish(&H, Hr);
+		g_checksum_get_digest(H, Hr, &digest_len);
+		g_checksum_free(H);
+
 		for (i = 0; i < 16; i++) {
 			sprintf(Hh + i * 2, "%02x", Hr[i]);
 		}

storage_xml.c
@@ -27,7 +27,6 @@
 #include "bitlbee.h"
 #include "base64.h"
 #include "arc.h"
-#include "md5.h"
 #include "xmltree.h"
 
 #include <glib/gstdio.h>
@@ -322,10 +321,11 @@ struct xt_node *xml_generate(irc_t *irc)
 {
 	char *pass_buf = NULL;
 	account_t *acc;
-	md5_byte_t pass_md5[21];
-	md5_state_t md5_state;
+	guint8 pass_md5[21];
+	GChecksum *md5_state;
 	GSList *l;
 	struct xt_node *root, *cur;
+	gsize digest_len = MD5_HASH_SIZE;
 
 	root = cur = xt_new_node("user", NULL, NULL);
 	if (irc->auth_backend) {
@@ -335,10 +335,11 @@ struct xt_node *xml_generate(irc_t *irc)
 		   (to prevent dictionary lookups of passwords) to end up with a 21-
 		   byte password hash, more convenient for base64 encoding. */
 		random_bytes(pass_md5 + 16, 5);
-		md5_init(&md5_state);
-		md5_append(&md5_state, (md5_byte_t *) irc->password, strlen(irc->password));
-		md5_append(&md5_state, pass_md5 + 16, 5);   /* Add the salt. */
-		md5_finish(&md5_state, pass_md5);
+		md5_state = g_checksum_new(G_CHECKSUM_MD5);
+		g_checksum_update(md5_state, (guint8 *) irc->password, strlen(irc->password));
+		g_checksum_update(md5_state, pass_md5 + 16, 5);   /* Add the salt. */
+		g_checksum_get_digest(md5_state, pass_md5, &digest_len);
+		g_checksum_free(md5_state);
 		/* Save the hash in base64-encoded form. */
 		pass_buf = base64_encode(pass_md5, 21);
 		xt_add_attr(cur, "password", pass_buf);

commit cc4d055a832898370e30a7501be244e20f612582

jabber: fix regression with gchecksum in jabber_cache_add
This crashed with "The checksum '%s' has been closed and cannot be
updated anymore." (which is also a user after free)

Issue introduced in 1bdc6696aefb617873b56001f81d649d6ca3aa8e which is
not in any released version.

There was this md5_digest_keep() function that freed a copy of the
checksum instead of the one you pass. The code that it was replaced with
ignored this distinction, and since jabber_cache_add() needs to reuse the
GChecksum this led to this issue.

No other place in the bitlbee code used this function.

protocols/jabber/jabber_util.c
@@ -139,7 +139,7 @@ void jabber_cache_add(struct im_connection *ic, struct xt_node *node, jabber_cac
 {
 	struct jabber_data *jd = ic->proto_data;
 	struct jabber_cache_entry *entry = g_new0(struct jabber_cache_entry, 1);
-	GChecksum *id_hash;
+	GChecksum *id_hash, *copy;
 	gsize digest_len = MD5_HASH_SIZE;
 	guint8 id_sum[MD5_HASH_SIZE];
 	char *id, *asc_hash;
@@ -148,8 +148,10 @@ void jabber_cache_add(struct im_connection *ic, struct xt_node *node, jabber_cac
 
 	id_hash = jd->cached_id_prefix;
 	g_checksum_update(id_hash, (guint8 *) &next_id, sizeof(next_id));
-	g_checksum_get_digest(id_hash, id_sum, &digest_len);
-	g_checksum_free(id_hash);
+
+	copy = g_checksum_copy(id_hash);
+	g_checksum_get_digest(copy, id_sum, &digest_len);
+	g_checksum_free(copy);
 
 	asc_hash = base64_encode(id_sum, 12);
 