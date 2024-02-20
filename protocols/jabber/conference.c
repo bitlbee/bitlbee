bitlbee/bitlbee

commit a4ac9c413b97decae5adf15b69f4bbfdce0ef5e4

Deprecate sha1_* functions (#172)
* Migrate sha1 calls to direct use of GChecksum

* Mark sha1.h functions as deprecated

protocols/jabber/conference.c
@@ -22,7 +22,6 @@
 \***************************************************************************/
 
 #include "jabber.h"
-#include "sha1.h"
 
 static xt_status jabber_chat_join_failed(struct im_connection *ic, struct xt_node *node, struct xt_node *orig);
 static xt_status jabber_chat_self_message(struct im_connection *ic, struct xt_node *node, struct xt_node *orig);
@@ -78,15 +77,15 @@ struct groupchat *jabber_chat_with(struct im_connection *ic, char *who)
 	struct jabber_data *jd = ic->proto_data;
 	struct jabber_chat *jc;
 	struct groupchat *c;
-	sha1_state_t sum;
+	GChecksum *sum;
 	double now = gettime();
 	char *uuid, *rjid, *cserv;
 
-	sha1_init(&sum);
-	sha1_append(&sum, (uint8_t *) ic->acc->user, strlen(ic->acc->user));
-	sha1_append(&sum, (uint8_t *) &now, sizeof(now));
-	sha1_append(&sum, (uint8_t *) who, strlen(who));
-	uuid = sha1_random_uuid(&sum);
+	sum = g_checksum_new(G_CHECKSUM_SHA1);
+	g_checksum_update(sum, (uint8_t *) ic->acc->user, strlen(ic->acc->user));
+	g_checksum_update(sum, (uint8_t *) &now, sizeof(now));
+	g_checksum_update(sum, (uint8_t *) who, strlen(who));
+	uuid = sha1_random_uuid(sum);
 
 	if (jd->flags & JFLAG_GTALK) {
 		cserv = g_strdup("groupchat.google.com");

lib/sha1.c
@@ -99,8 +99,11 @@ char *sha1_random_uuid(sha1_state_t * context)
 	guint8 dig[SHA1_HASH_SIZE];
 	char *ret = g_new0(char, 40);   /* 36 chars + \0 */
 	int i, p;
+	gsize digest_len = SHA1_HASH_SIZE;
 
-	sha1_finish(context, dig);
+	g_checksum_get_digest(*context, dig, &digest_len);
+	g_checksum_free(*context);
+
 	for (p = i = 0; i < 16; i++) {
 		if (i == 4 || i == 6 || i == 8 || i == 10) {
 			ret[p++] = '-';

lib/sha1.h
@@ -5,14 +5,20 @@
 #include <glib.h>
 #include <gmodule.h>
 
+#ifdef __GNUC__
+#define __SHA1_NON_PUBLIC_DEPRECATION__ __attribute__((deprecated("sha1.h will be removed from Bitlbee's public API. Please use another library (such as GLib's gchecksum) instead")))
+#else
+#define __SHA1_NON_PUBLIC_DEPRECATION__
+#endif
+
 #define SHA1_HASH_SIZE 20
 
 typedef GChecksum *sha1_state_t;
 
-void sha1_init(sha1_state_t *);
-void sha1_append(sha1_state_t *, const guint8 *, unsigned int);
-void sha1_finish(sha1_state_t *, guint8 digest[SHA1_HASH_SIZE]);
-void sha1_hmac(const char *, size_t, const char *, size_t, guint8 digest[SHA1_HASH_SIZE]);
+void sha1_init(sha1_state_t *) __SHA1_NON_PUBLIC_DEPRECATION__;
+void sha1_append(sha1_state_t *, const guint8 *, unsigned int) __SHA1_NON_PUBLIC_DEPRECATION__;
+void sha1_finish(sha1_state_t *, guint8 digest[SHA1_HASH_SIZE]) __SHA1_NON_PUBLIC_DEPRECATION__;
+void sha1_hmac(const char *, size_t, const char *, size_t, guint8 digest[SHA1_HASH_SIZE]) ;
 char *sha1_random_uuid(sha1_state_t *);
 
 #endif

protocols/jabber/iq.c
@@ -22,7 +22,6 @@
 \***************************************************************************/
 
 #include "jabber.h"
-#include "sha1.h"
 
 static xt_status jabber_parse_roster(struct im_connection *ic, struct xt_node *node, struct xt_node *orig);
 static xt_status jabber_iq_display_vcard(struct im_connection *ic, struct xt_node *node, struct xt_node *orig);
@@ -244,15 +243,17 @@ static xt_status jabber_do_iq_auth(struct im_connection *ic, struct xt_node *nod
 	if (xt_find_node(query->children, "digest") && (s = xt_find_attr(jd->xt->root, "id"))) {
 		/* We can do digest authentication, it seems, and of
 		   course we prefer that. */
-		sha1_state_t sha;
+		GChecksum *sha;
 		char hash_hex[41];
 		unsigned char hash[20];
 		int i;
+		gsize digest_len = 20;
 
-		sha1_init(&sha);
-		sha1_append(&sha, (unsigned char *) s, strlen(s));
-		sha1_append(&sha, (unsigned char *) ic->acc->pass, strlen(ic->acc->pass));
-		sha1_finish(&sha, hash);
+		sha = g_checksum_new(G_CHECKSUM_SHA1);
+		g_checksum_update(sha, (unsigned char *) s, strlen(s));
+		g_checksum_update(sha, (unsigned char *) ic->acc->pass, strlen(ic->acc->pass));
+		g_checksum_get_digest(sha, hash, &digest_len);
+		g_checksum_free(sha);
 
 		for (i = 0; i < 20; i++) {
 			sprintf(hash_hex + i * 2, "%02x", hash[i]);

protocols/jabber/si.c
@@ -22,7 +22,6 @@
 \***************************************************************************/
 
 #include "jabber.h"
-#include "sha1.h"
 
 void jabber_si_answer_request(file_transfer_t *ft);
 int jabber_si_send_request(struct im_connection *ic, char *who, struct jabber_transfer *tf);

commit ff3f4350ab728c2a8fcecb84487deede4042f46a

Adjust sha1_random_uuid usage in jabber/conference.c
The jabber/conference.c module failed to compile due to the implicit use
of the sha1_random_uuid function following the deprecation of the sha1
lib. This patch reintroduces the sha1.h include to resolve compilation
errors encountered with GCC 14:

  error: implicit declaration of function ‘sha1_random_uuid’

Additionally, the patch corrects the type mismatch error when calling
sha1_random_uuid, aligning with the expected ‘GChecksum **’ type,
otherwise compilation fails with following error:

  error: passing argument 1 of ‘sha1_random_uuid’ from incompatible pointer type
  note: expected ‘GChecksum **’ {aka ‘struct _GChecksum **’} but argument is of type ‘GChecksum *’ {aka ‘struct _GChecksum *’}

Fixes: a4ac9c413b97 ("Deprecate sha1_* functions (#172)")

protocols/jabber/conference.c
@@ -22,6 +22,7 @@
 \***************************************************************************/
 
 #include "jabber.h"
+#include "sha1.h"
 
 static xt_status jabber_chat_join_failed(struct im_connection *ic, struct xt_node *node, struct xt_node *orig);
 static xt_status jabber_chat_self_message(struct im_connection *ic, struct xt_node *node, struct xt_node *orig);
@@ -85,7 +86,7 @@ struct groupchat *jabber_chat_with(struct im_connection *ic, char *who)
 	g_checksum_update(sum, (uint8_t *) ic->acc->user, strlen(ic->acc->user));
 	g_checksum_update(sum, (uint8_t *) &now, sizeof(now));
 	g_checksum_update(sum, (uint8_t *) who, strlen(who));
-	uuid = sha1_random_uuid(sum);
+	uuid = sha1_random_uuid(&sum);
 
 	if (jd->flags & JFLAG_GTALK) {
 		cserv = g_strdup("groupchat.google.com");